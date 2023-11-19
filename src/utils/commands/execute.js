63883e045958e0b4174a33544d20a659afb7f5ab
madhukar123456 <shroudmg@gmail.com>
2023-11-19 09:49:13 +0545

    update start script

--- src/utils/commands/execute.js
+++ src/utils/commands/execute.js
@@ -1,6 +1,10 @@
 import { runTerminalCommands } from "../../run_terminal.js";
 export async function runExecuteCommands(targetFolder){
   runTerminalCommands([
+    'pip install kaggle',
+    'mkdir ~/.kaggle',
+    'cp kaggle.json ~/.kaggle/',
+    'chmod 600 ~/.kaggle/kaggle.json',
    `kaggle --version`, 
   `kaggle kernels init -p /${targetFolder}`]);
 }
